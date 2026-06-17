2a5c57359f8dfb5e61ef25ba2a00d1cbd7871f90
nicoladen05 <me@nicoladen.dev>
2026-06-17 12:48:38 +0200

    add selection menu when suggesting a mod

--- src/commands/modpack/suggest.ts
+++ src/commands/modpack/suggest.ts
@@ -1,6 +1,13 @@
-import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+import {
+  ActionRowBuilder,
+  ChatInputCommandInteraction,
+  MessageFlags,
+  SlashCommandBuilder,
+  StringSelectMenuBuilder,
+  StringSelectMenuOptionBuilder,
+} from "discord.js";
 import type { Command } from "../../client";
-import { modpack } from "../..";
+import { ModrinthProject, searchModrinthProjects } from "../../lib/modrinth";
 
 export default {
   data: new SlashCommandBuilder()
@@ -8,18 +15,48 @@ export default {
     .setDescription("Suggest a mod to add to the modpack")
     .addStringOption((option) =>
       option
-        .setName("modrinth-slug")
-        .setDescription("The slug of the mod on modrinth")
+        .setName("search-query")
+        .setDescription("The search query to use for finding a mod")
         .setRequired(true),
     ),
 
   execute: async (interaction: ChatInputCommandInteraction) => {
-    interaction.deferReply();
+    await interaction.deferReply({ flags: MessageFlags.Ephemeral });
 
-    await modpack.addModrinthMod(
-      interaction.options.getString("modrinth-slug", true),
-    );
-
-    interaction.editReply("Mod suggestion accepted");
+    await processSubmission(interaction);
   },
 } satisfies Command;
+
+async function processSubmission(interaction: ChatInputCommandInteraction) {
+  const searchQuery = interaction.options.get("search-query", true)
+    .value as string;
+
+  const searchResults = await searchModrinthProjects(searchQuery);
+
+  if (searchResults.length > 1) {
+    const row = new ActionRowBuilder<StringSelectMenuBuilder>().addComponents(
+      buildSelectionMenu(searchResults),
+    );
+
+    await interaction.followUp({
+      content: "Choose the correct mod",
+      components: [row],
+      flags: MessageFlags.Ephemeral,
+    });
+  }
+}
+
+function buildSelectionMenu(
+  projects: ModrinthProject[],
+): StringSelectMenuBuilder {
+  return new StringSelectMenuBuilder()
+    .setCustomId("project-select")
+    .setPlaceholder("Select a project")
+    .addOptions(
+      ...projects.map((project) =>
+        new StringSelectMenuOptionBuilder()
+          .setLabel(project.title ?? "Unknown Mod")
+          .setValue(project.project_id),
+      ),
+    );
+}
---
+++ src/lib/modrinth.ts
@@ -0,0 +1,91 @@
+const USER_AGENT = "nicoladen05/packwiz-suggestion-bot";
+
+export type ModrinthProject = {
+  project_id: string;
+  project_type: "mod" | "modpack" | "resourcepack" | "shader";
+  slug?: string;
+  author: string;
+  title?: string;
+  description?: string;
+  categories?: string[];
+  icon_url?: string;
+  color?: number;
+  client_side?: "required" | "optional" | "unsupported" | "unknown";
+  server_side?: "required" | "optional" | "unsupported" | "unknown";
+  downloads: number;
+  display_categories?: string[];
+  versions: string[];
+  latest_version?: string;
+  gallery?: string[];
+  featured_gallery?: string[];
+};
+
+export type ModrinthFacets = {
+  type?: "mod" | "resourcepack" | "shader";
+  loaders?: ("fabric" | "forge" | "neoforge")[];
+  versions?: string[];
+  client_side?: "required" | "optional" | "unsupported";
+  server_side?: "required" | "optional" | "unsupported";
+};
+
+/**
+ * Searches for modpacks on Modrinth.
+ * @param searchQuery The search query to use.
+ * @returns The 10 most relevant search results
+ */
+export async function searchModrinthProjects(
+  searchQuery: string,
+  facets?: ModrinthFacets,
+): Promise<ModrinthProject[]> {
+  const queryParams = new URLSearchParams({
+    query: searchQuery,
+  });
+
+  if (facets) {
+    queryParams.append("facets", parseFacets(facets));
+  }
+
+  const response = await fetch(
+    `https://api.modrinth.com/v2/search?${queryParams}`,
+    {
+      headers: {
+        "User-Agent": USER_AGENT,
+        "Content-Type": "application/json",
+      },
+    },
+  );
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    return [];
+  }
+
+  return data.hits satisfies ModrinthProject[];
+}
+
+function parseFacets(facets: ModrinthFacets): string {
+  let facetStrings = [];
+
+  if (facets.type) facetStrings.push(`["project_type:${facets.type}"]`);
+
+  if (facets.loaders) {
+    const loaders = facets.loaders.map((loader) => `"categories:${loader}"`);
+
+    facetStrings.push(`[${loaders.join(",")}]`);
+  }
+
+  if (facets.versions) {
+    const versions = facets.versions.map((version) => `"versions:${version}"`);
+
+    facetStrings.push(`[${versions.join(",")}]`);
+  }
+
+  if (facets.client_side)
+    facetStrings.push(`["client_side:${facets.client_side}"]`);
+
+  if (facets.server_side)
+    facetStrings.push(`["server_side:${facets.server_side}"]`);
+
+  return `[${facetStrings.join(",")}]`;
+}
