489758bd1fd5814d7c48a7c6335e02bf16a623ae
nicoladen05 <me@nicoladen.dev>
2026-06-20 19:34:35 +0200

    refactor setup modal

--- src/commands/admin/setup.ts
+++ src/commands/admin/setup.ts
@@ -2,10 +2,12 @@ import {
   ChannelSelectMenuBuilder,
   ChannelType,
   ChatInputCommandInteraction,
+  InteractionContextType,
   LabelBuilder,
   MessageFlags,
   ModalBuilder,
   ModalSubmitInteraction,
+  PermissionFlagsBits,
   SlashCommandBuilder,
   TextDisplayBuilder,
   TextInputBuilder,
@@ -19,85 +21,33 @@ import { eq } from "drizzle-orm";
 export default {
   data: new SlashCommandBuilder()
     .setName("setup")
-    .setDescription("Configure the bot"),
+    .setDescription("Configure the bot")
+    .setContexts(InteractionContextType.Guild),
   // .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
 
   execute: async (interaction: ChatInputCommandInteraction) => {
+    if (!interaction.guild)
+      throw new Error("The command was not executed in a guild");
+
+    const {
+      isConfigured: isAlreadyConfigured,
+      packwizUrl: currentPackwizUrl,
+      channel: currentChannel,
+    } = await isGuildAlreadyConfigured(interaction.guild.id);
+
     const modal = new ModalBuilder().setCustomId("setup").setTitle("Setup");
 
-    if (!interaction.guild) return;
-
-    // Already configured warning
-    const serverFromDb = (
-      await db
-        .select({ packwizUrl: modpack.url, channel: server.pollChannel })
-        .from(server)
-        .where(eq(server.serverId, interaction.guild.id))
-        .innerJoin(modpack, eq(server.serverId, modpack.serverId))
-        .limit(1)
-    )[0];
-
-    if (serverFromDb) {
-      const alreadyConfiguredLabel = new TextDisplayBuilder().setContent(
-        "**⚠️ This server is already configured!**\nThe current config will be overwritten, if you submit the setup again.",
-      );
-
-      modal.addTextDisplayComponents(alreadyConfiguredLabel);
+    if (isAlreadyConfigured) {
+      modal.addTextDisplayComponents(buildAlreadyConfiguredTextDisplay());
     }
 
-    // Packwiz URL input
-    const packUrlInput = new TextInputBuilder()
-      .setCustomId("packwizURL")
-      .setStyle(TextInputStyle.Short)
-      .setPlaceholder("https://github.com/")
-      .setRequired(true);
-
-    if (serverFromDb) {
-      packUrlInput.setValue(serverFromDb.packwizUrl);
-    }
-
-    const packUrlLabel = new LabelBuilder()
-      .setLabel("Packwiz URL")
-      .setDescription(
-        "The URL of the git repository that is hosting the packwiz modpack (must have pack.toml at the root).",
-      )
-      .setTextInputComponent(packUrlInput);
-
-    modal.addLabelComponents(packUrlLabel);
-
-    // GitHub access token input
-    const githubAccessTokenInput = new TextInputBuilder()
-      .setCustomId("githubAccessToken")
-      .setStyle(TextInputStyle.Short)
-      .setPlaceholder("github_pat_...")
-      .setRequired(true);
-
-    const githubAccessTokenLabel = new LabelBuilder()
-      .setLabel("GitHub access token")
-      .setDescription(
-        "A personal access token with write access to the modpack repository.",
-      )
-      .setTextInputComponent(githubAccessTokenInput);
-
-    modal.addLabelComponents(githubAccessTokenLabel);
-
-    // Channel input
-    const channelSelect = new ChannelSelectMenuBuilder()
-      .setCustomId("channel")
-      .setPlaceholder("Select a channel")
-      .setRequired(true)
-      .setChannelTypes(ChannelType.GuildText);
-
-    if (serverFromDb) {
-      channelSelect.setDefaultChannels(serverFromDb.channel);
-    }
-
-    const channelLabel = new LabelBuilder()
-      .setLabel("Channel")
-      .setDescription("The channel to send polls to.")
-      .setChannelSelectMenuComponent(channelSelect);
-
-    modal.addLabelComponents(channelLabel);
+    modal.addLabelComponents(
+      buildPackwizUrlInputLabel(
+        isAlreadyConfigured ? currentPackwizUrl : undefined,
+      ),
+      buildGithubAccessTokenInputLabel(),
+      buildChannelInputLabel(isAlreadyConfigured ? currentChannel : undefined),
+    );
 
     await interaction.showModal(modal);
   },
@@ -157,4 +107,88 @@ export async function handleSetupModalSubmit(
     content: "✅ Setup complete",
     flags: MessageFlags.Ephemeral,
   });
+}
+
+async function isGuildAlreadyConfigured(guildId: string): Promise<{
+  isConfigured: boolean;
+  packwizUrl: string | undefined;
+  channel: string | undefined;
+}> {
+  const serverFromDb = (
+    await db
+      .select({ packwizUrl: modpack.url, channel: server.pollChannel })
+      .from(server)
+      .where(eq(server.serverId, guildId))
+      .innerJoin(modpack, eq(server.serverId, modpack.serverId))
+      .limit(1)
+  )[0];
+
+  return {
+    isConfigured: !!serverFromDb,
+    packwizUrl: serverFromDb?.packwizUrl,
+    channel: serverFromDb?.channel,
+  };
+}
+
+function buildAlreadyConfiguredTextDisplay() {
+  return new TextDisplayBuilder().setContent(
+    "**⚠️ This server is already configured!**\nThe current config will be overwritten, if you submit the setup again.",
+  );
+}
+
+function buildPackwizUrlInputLabel(currentValue: string | undefined) {
+  const packUrlInput = new TextInputBuilder()
+    .setCustomId("packwizURL")
+    .setStyle(TextInputStyle.Short)
+    .setPlaceholder("https://github.com/")
+    .setRequired(true);
+
+  if (currentValue) {
+    packUrlInput.setValue(currentValue);
+  }
+
+  const packUrlLabel = new LabelBuilder()
+    .setLabel("Packwiz URL")
+    .setDescription(
+      "The URL of the git repository that is hosting the packwiz modpack (must have pack.toml at the root).",
+    )
+    .setTextInputComponent(packUrlInput);
+
+  return packUrlLabel;
+}
+
+function buildGithubAccessTokenInputLabel() {
+  const githubAccessTokenInput = new TextInputBuilder()
+    .setCustomId("githubAccessToken")
+    .setStyle(TextInputStyle.Short)
+    .setPlaceholder("github_pat_...")
+    .setRequired(true);
+
+  const githubAccessTokenLabel = new LabelBuilder()
+    .setLabel("GitHub access token")
+    .setDescription(
+      "A personal access token with write access to the modpack repository.",
+    )
+    .setTextInputComponent(githubAccessTokenInput);
+
+  return githubAccessTokenLabel;
+}
+
+function buildChannelInputLabel(currentValue: string | undefined) {
+  const channelSelect = new ChannelSelectMenuBuilder()
+    .setCustomId("channel")
+    .setPlaceholder("Select a channel")
+    .setRequired(true)
+    .setChannelTypes(ChannelType.GuildText);
+
+  if (currentValue) {
+    channelSelect.setDefaultChannels(currentValue);
+  }
+
+  const channelLabel = new LabelBuilder()
+    .setLabel("Channel")
+    .setDescription("The channel to send polls to.")
+    .setChannelSelectMenuComponent(channelSelect);
+
+  return channelLabel;
 }
